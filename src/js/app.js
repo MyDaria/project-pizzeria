a0978cfabd61346a3c7a157bd33c1c27b183adf0
DARIA_MYSZA <dariamysza.pv@op.pl>
2022-10-22 17:46:56 +0200

    Module 10.2

--- src/js/app.js
+++ src/js/app.js
@@ -84,14 +84,14 @@ const app = {
         return rawResponse.json();
       })
       .then(function(parsedResponse){
-        console.log('parsedResponse', parsedResponse);
+        //console.log('parsedResponse', parsedResponse);
 
         thisApp.data.products = parsedResponse;
           
         thisApp.initMenu();
       });
 
-    console.log('thisApp.data', JSON.stringify(thisApp.data));
+    //console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
   initBooking: function(){
